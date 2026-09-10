1870427e0c0540b292b1c12aa4862cb5ac2a66ab
Lucas Benfatti <lucasabenfatti@gmail.com>
2026-09-10 19:30:31 -0300

    test(domain): comprehensive test suite for Pessoa entity

--- crud-cadastro/tests/test_pessoa.py
+++ crud-cadastro/tests/test_pessoa.py
@@ -1,6 +1,6 @@
 import pytest
 from domain import Pessoa
-from exceptions import IdadeInvalidaError
+from exceptions import IdadeInvalidaError, NomeInvalidoError, NomeIncompletoError, NomeComCaracteresInvalidosError
 
 @pytest.mark.parametrize("idade_valida", [0, 18, 130])
 def test_pessoa_aceita_idade_valida(idade_valida):
@@ -11,3 +11,42 @@ def test_pessoa_aceita_idade_valida(idade_valida):
 def test_pessoa_rejeita_idade_invalida(idade_invalida):
     with pytest.raises(IdadeInvalidaError):
         Pessoa(None, "Lucas Benfatti", idade_invalida)
+
+@pytest.mark.parametrize("nome_nao_normalizado", ["lucas   benfatti", "lucas Benfatti  "])
+def test_pessoa_normaliza_nome_completo(nome_nao_normalizado):
+    pessoa = Pessoa(None, nome_nao_normalizado, 20)
+    assert pessoa.nome == "Lucas Benfatti"
+
+@pytest.mark.parametrize("primeiro_nome_valido", ["A" + "a" * 2 + " Pereira", "A" + "a" * 31 + " Pereira"])
+def test_pessoa_aceita_primeiro_nome_valido(primeiro_nome_valido):
+    pessoa = Pessoa(None, primeiro_nome_valido, 20)
+    assert pessoa.nome == primeiro_nome_valido
+
+@pytest.mark.parametrize("primeiro_nome_invalido", ["a" * 2 + " Pereira", "a" * 33 + " Pereira"])
+def test_pessoa_rejeita_primeiro_nome_invalido(primeiro_nome_invalido):
+    with pytest.raises(NomeInvalidoError):
+        Pessoa(None, primeiro_nome_invalido, 20)
+
+def test_pessoa_aceita_primeiro_nome_acentuado_valido():
+    pessoa = Pessoa(None, "Áâãàç Pereira", 20)
+    assert pessoa.nome == "Áâãàç Pereira"
+
+@pytest.mark.parametrize("sobrenome_valido", ["Lucas " + "A" + "a", "Lucas " + "A" + "a" * 49])
+def test_pessoa_aceita_sobrenome_valido(sobrenome_valido):
+    pessoa = Pessoa(None, sobrenome_valido, 20)
+    assert pessoa.nome == sobrenome_valido
+
+@pytest.mark.parametrize("sobrenome_invalido", ["Lucas " + "a" * 1, "Lucas " + "a" * 51])
+def test_pessoa_rejeita_sobrenome_invalido(sobrenome_invalido):
+    with pytest.raises(NomeInvalidoError):
+        Pessoa(None, sobrenome_invalido, 20)
+
+@pytest.mark.parametrize("nome_incompleto", ["Lucas", "Mauro"])
+def test_pessoa_rejeita_nome_incompleto(nome_incompleto):
+    with pytest.raises(NomeIncompletoError):
+        Pessoa(None, nome_incompleto, 20)
+
+@pytest.mark.parametrize("nome_com_caracteres_invalidos", ["Luc4s Benfatti", "Luc@s Benf4tti", "Luc#s Benfatti"])
+def test_pessoa_rejeita_caracteres_invalidos(nome_com_caracteres_invalidos):
+    with pytest.raises(NomeComCaracteresInvalidosError):
+        Pessoa(None, nome_com_caracteres_invalidos, 20)
