0cff49b66e792fa4898fc865d157310915371496
Lucas Benfatti <lucasabenfatti@gmail.com>
2026-09-11 18:43:46 -0300

    test(service): add unit tests for person listing

--- crud-cadastro/tests/test_pessoa_service.py
+++ crud-cadastro/tests/test_pessoa_service.py
@@ -20,3 +20,18 @@ def test_pessoa_service_aceita_cadastro() -> None:
     pessoa = Pessoa(None, "Lucas Benfatti", 18)
     fake_id = pessoa_service.cadastrar(pessoa)
     assert fake_id == 1
+
+def test_pessoa_service_lista_pessoas() -> None:
+    fake_repository = FakeRepository()
+    pessoa_service = PessoaService(fake_repository)
+    lista_pessoas_esperada = []
+    pessoa_1 = Pessoa(None, "Lucas Benfatti", 18)
+    pessoa_2 = Pessoa(None, "John Doe", 40)
+    lista_pessoas_esperada.append(pessoa_1)
+    lista_pessoas_esperada.append(pessoa_2)
+    fake_id = pessoa_service.cadastrar(pessoa_1)
+    pessoa_1.registrar_persistencia(fake_id)
+    fake_id = pessoa_service.cadastrar(pessoa_2)
+    pessoa_2.registrar_persistencia(fake_id)
+    lista_pessoas = pessoa_service.listar()
+    assert lista_pessoas == lista_pessoas_esperada
